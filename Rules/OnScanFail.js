a77f99d9006e52fc8842eba30aff3c5fd20a3eeb
Robson Ferreira de Melo <c0712303@vale.com>
2025-09-04 18:13:35 +0000

    Criação Entrada de Loja do Kit e remoção de arquivos obsoletos

--- Rules/OnScanFail.js
+++
@@ -1,6 +0,0 @@
-export default function OnScanFail(clientAPI) {
-    return clientAPI.executeAction({
-        "_Type": "Action.Type.Message",
-        "Message": "Falha ao escanear o código"
-    });
-}
